d73ca9ff6ae1236f227aea2f185377bbbfd87ae9
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-29 13:48:36 -0700

    Add delegate for customer methods on invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,13 @@
 class Invoice < ActiveRecord::Base
   belongs_to :customer
+
+
+  delegate :name,
+    :street,
+    :city,
+    :state,
+    :zip_code,
+    :to => :customer,
+    :prefix => true
+
 end
